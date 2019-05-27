742549bd31ab03118a3981b897b62c93d01ffa5c
Chiheb Bouzid <chihebouzid@gmail.com>
2019-05-27 11:06:54 +0100

    build the shopping list model

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
+import List from './models/List';
 import { element, renderLoader, clearLoader } from './views/base';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
@@ -119,3 +120,4 @@ element.recipePage.addEventListener('click', e => {
     recipeView.updateServingsIngredients(state.recipe);
 })
 
+window.list = new List();
